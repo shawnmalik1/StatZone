89792d7721f68c8e24b02e990b3c1dfeaa4862ea
Shawn <wbr4az@virginia.edu>
2024-03-08 00:57:17 -0500

    changed file name

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import responses
-import JSON
+import commands
 
 async def send_message(message, user_message):
     try:
